c4adea140c1cf2e3f2b1ecea48b628f1d1d046ce
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-02 16:48:54 +0530

    Removed Empty line

--- app/src/main/java/com/example/onlineproducts/products/OnProductActionListener.java
+++ app/src/main/java/com/example/onlineproducts/products/OnProductActionListener.java
@@ -1,7 +1,5 @@
 package com.example.onlineproducts.products;
 
-import com.example.onlineproducts.models.Product;
-
 public interface OnProductActionListener {
 
     void onProductClick(int productId);
